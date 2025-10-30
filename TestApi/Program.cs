9193bc1bfd0e5a1c5e4a4ee3efbbea0695e1ce69
Roman Sadovskiy <s03roman@mail.ru>
2025-10-30 20:58:33 +0300

    Rename

--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "JWTToken_Auth_API",
+        Title = "TestApi",
         Version = "v1"
     });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
